6f4269a22e5b9d98dd40de875de402c8d0388823
joeban <joeanddaneil5@gmail.com>
2025-08-07 13:15:43 +0800

    db auto migrate

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import * as auth from '$lib/server/auth';
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleServerError, ServerInit } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import { db } from '$lib/server/db';
+import path from 'path';
 
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -34,4 +37,29 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
+function _init(): ServerInit {
+	return async () => {
+		try {
+			await migrate(db, { migrationsFolder: path.resolve('.') + '/migrations' });
+		} catch (error) {
+			console.error('Migration failed:', error);
+			process.exit(1);
+		}
+	};
+}
+
+function error(): HandleServerError {
+	return async ({ error, event, status, message }) => {
+		console.error('Error occurred:', error, event, status, message);
+
+		return {
+			status: 'error',
+			code: status,
+			message
+		};
+	};
+}
+
 export const handle: Handle = sequence(handleParaglide, handleAuth);
+export const init: ServerInit = _init();
+export const handleError: HandleServerError = error();
